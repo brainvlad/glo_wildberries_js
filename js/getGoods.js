3cb81468fbecabe6eb85193019397c74533a35c2
brainvlad <wladiik@gmail.com>
2021-11-21 16:43:48 +0300

    add task day 3

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,25 +1,74 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
+    const viewAll = document.querySelector('.more')
+
+    // отрисовка товаров
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}"	class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>	
+                </div>			
+            `
+
+            goodsContainer.append(goodBlock)
+        })
+    }
 
     // получаем данный с сервера (в данном случае локального)
-    const getData = () => {
+    const getData = (value, category) => {
         fetch('db/db.json')
             .then((res) => res.json())
             .then((data) => {
-                localStorage.setItem('goods', JSON.stringify(data))
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array))
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array)
+                }
             })
     }
 
     // события при клике по ссылке
-    // links.forEach((link) => {
-    //     link.addEventListener('click', (event) => {
-    //         event.preventDefault()
-    //         getData()
-    //     })
-    // })
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const linkValue = link.textContent
+            const category = link.dataset.field
 
-    const goods = JSON.parse(localStorage.getItem('goods'))
-    console.log(localStorage)
+            getData(linkValue, category)
+        })
+    })
+
+    if (viewAll) {
+        viewAll.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            getData()
+        })
+    }
+
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
 }
 
 getGoods()
